a814419fd472935c56eace8e24f27f246b5856f0
GalSarid21 <galsarid@gmail.com>
2021-10-21 10:40:47 +0300

    load project to git

---
+++ ingest/views.py
@@ -0,0 +1,14 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import ScanSerializer
+
+
+@api_view(['POST'])
+def create_new_scan(request):
+    try:
+        serializer = ScanSerializer(data = request.data)
+        serializer.is_valid()
+        serializer.save()
+        return Response()
+    except Exception as ex:
+        return Response(f"Creation of new scan failed with exception: {ex}")
---
+++ scanner/consts.py
@@ -0,0 +1,7 @@
+
+class Consts:
+    ACCEPTED = 'AC'
+    RUNNING = 'RU'
+    ERROR = 'ER'
+    COMPLETE = 'CO'
+    NOT_FOUND = 'NF'
